0156ce9d4346bb8dfbd9269878f9f948642d2599
chika <chika1086@gmail.com>
2019-11-29 12:25:14 +0900

    スレッドのCRUD機能を作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,43 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def new
+    @topic = Topic.new
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
+
+  def create
+    topic = Topic.new(topic_params)
+    topic.save!
+    redirect_to topics_url, notice: "スレッド「#{topic.title}」を投稿しました。"
+  end
+
+  def update
+    topic = Topic.find(params[:id])
+    topic.update!(topic_params)
+    redirect_to topics_url, notice: "スレッド「#{topic.title}」を更新しました。"
+  end
+
+  def destroy
+    topic = Topic.find(params[:id])
+    topic.destroy
+    redirect_to topics_url, notice: "スレッド「#{topic.title}」を削除しました。"
+  end
+
+  private
+
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
+
+  # private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-  get 'topics/show'
-  get 'topics/new'
-  get 'topics/edit'
+  root to: 'topics#index'
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
