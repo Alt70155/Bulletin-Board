8ee6d4bb325de69abbbc2015248457d224a4b702
chika <chika1086@gmail.com>
2019-12-02 18:17:28 +0900

    親コメントを消した場合、子コメントも削除するよう実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,8 +14,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    # 親コメントが削除された場合、それへの返信コメントも削除する
     topic = Topic.find(session[:topic_id])
     comment = Comment.find(params[:id])
+    reply_comment_destroy(topic, comment)
     comment.destroy
     redirect_to topic, notice: "コメントを削除しました。"
   end
@@ -24,6 +26,21 @@ class CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:name, :body, :reply_path)
+    end
+
+    def reply_comment_destroy(topic, parent_comment)
+      all_comments = topic.comments.all
+      # 掲示板内でのコメント番号を検索
+      p_comment_num = all_comments.index(parent_comment) + 1
+
+      # 自分のコメントへの返信コメントのパスを作成する
+      # 通常コメントなら自分の番号を、返信コメントならreply_path + 自分の番号
+      reply_path = p_comment_num.to_s
+      reply_path = "#{parent_comment.reply_path}/#{p_comment_num}" if parent_comment.reply_path.present?
+
+      all_comments.select { |comment|
+        comment.reply_path == reply_path || /#{reply_path.delete('/')}\d.*/ === comment.reply_path&.delete('/')
+      }.each(&:destroy)
     end
 
   # private
